cdc748accb8d12b331662a02252383263bd34dc9
Guillermo Gutiérrez <xiterrex@gmail.com>
2012-03-28 20:29:40 +0200

    Detecting file separator marks.

--- src/unupdatapp.c
+++ src/unupdatapp.c
@@ -1,8 +1,23 @@
+#include "config.h"
 #include <stdio.h>
+#include <stdint.h>
 #include <stdlib.h>
+#include <string.h>
 
-int main()
+#define FILE_SEPARATOR "\x55\xAA\x5A\xA5"
+
+int main(int argc, char* argv[])
 {
+    FILE *input = ((argc == 2 && strcmp(argv[1], "-") != 0) ? fopen(argv[1], "rb") : stdin);
+    uint8_t word[4];
+    while (fread(&word, sizeof(word), 1, input)) {
+        if (memcmp(word, FILE_SEPARATOR, sizeof(word)) == 0) {
+            printf("found file separator\n");
+        } else {
+            //printf("read instead %08x\n", word);
+        }
+    }
+    fclose(input);
     return 0;
 }
 
