b4933c0f9c049aebc16bc6efa751797047c22ac2
Guillermo Gutiérrez <xiterrex@gmail.com>
2012-04-14 19:02:49 +0200

    read crc saving memory

--- src/crc.c
+++ src/crc.c
@@ -11,25 +11,22 @@ crc16(const char *data, int length)
 {
     FILE *pipe;
     FILE *tmp;
-    //char *crc;
-    char crc[1000000];
     crc_t *result;
+    char byte;
     int i;
 
     result = calloc(sizeof(crc_t), 1);
+    result->crc = malloc(length / 1024 + 2);
     remove(TEMP_FILE_FOR_CRC_CHECK);
     tmp = fopen(TEMP_FILE_FOR_CRC_CHECK, "wb+");
     fwrite(data, sizeof(char), length, tmp);
     fclose(tmp);
     pipe = popen("./crc " TEMP_FILE_FOR_CRC_CHECK, "r");
-    //fscanf(pipe, "%as", crc);
-    fscanf(pipe, "%s", crc);
-    result->length = strlen(crc) / 2;
-    result->crc = malloc(result->length);
-    for(i = 0; i < result->length; i++) {
-        sscanf(crc + i * 2, "%X", result->crc + i);
+    for(i = 0; !feof(pipe); i++) {
+        fscanf(pipe, "%X", result->crc + i);
     }
-    //free(crc);
+    result->crc[i] = '\0';
+    result->length = i;
     fclose(pipe);
 
     return result;
